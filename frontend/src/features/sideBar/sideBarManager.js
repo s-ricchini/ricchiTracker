e17feb2b94a2a82d7509aa446588b192d1ab8754
unknown <ricchini.santiagotomas@gmail.com>
2026-03-06 21:45:19 -0300

    mejora de optimizacion para sideBarManager

--- frontend/src/features/sideBar/sideBarManager.js
+++ frontend/src/features/sideBar/sideBarManager.js
@@ -3,17 +3,16 @@ import SidebarNode from "./sideBarNode"
 class SideBarManager{
     constructor(data){
         this.dataPlana = data
-        this.map = {}
+        this.nodeMap = {}
         this.tree = this.createTree()
     }
 
     createTree(){
-        const nodeMap = {}
         const root = []
         
         //creo el mapa id : nodo O(n)
         this.dataPlana.forEach(element => {
-            nodeMap[element.id] = new SidebarNode(element)            
+            this.nodeMap[element.id] = new SidebarNode(element)            
         });
 
 
@@ -21,29 +20,28 @@ class SideBarManager{
         this.dataPlana.forEach(element => {
             //es el hijo de alguien
             if (element.parent_id){
-                const nodoPadre = nodeMap[element.parent_id];
-                const nodoHijo = nodeMap[element.id];
+                const nodoPadre = this.nodeMap[element.parent_id];
+                const nodoHijo = this.nodeMap[element.id];
 
                 nodoPadre.addChild(nodoHijo);
 
             } else {
                 //no tiene padre -> es root
-                root.push(nodeMap[element.id])
+                root.push(this.nodeMap[element.id])
             }
         });
 
         //ordeno por posicion los elementos tambien del root
+
         return root.sort((a, b) => a.position - b.position)
         
     }
 
-    //crear arbol
-        // 
-
-
-    //agregar nodo a arbol existente (nodo)
-
-    //crear mapa de la flatlist
-
+    //metodo para encontrar por id
+    findById(id){
+        return this.nodeMap[id];
+    }
 
 }
+
+export default SideBarManager
